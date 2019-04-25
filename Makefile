164c0a108290a7628410d4eb607420ca11c726cf
Will Sams <willsams@hotmail.com>
2019-04-25 08:02:32 -0400

    Began cleaning up code, 2 years later.  Added 'symbols' and 'dump' available for make.

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ GENDEV=/opt/m68k
 AS	= $(GENDEV)/bin/m68k-elf-as
 LD	= $(GENDEV)/bin/m68k-elf-ld
 
+READELF = $(GENDEV)/bin/m68k-elf-readelf
+OBJDUMP = $(GENDEV)/bin/m68k-elf-objdump
 DEBUGGER = $(GENDEV)/tools/dgen
 
 ASFLAGS = -m68000 --register-prefix-optional 
@@ -24,7 +26,13 @@ $(BIN): $(OBJS)
 %.o: %.s 
 	@echo "AS $<"
 	@$(AS) $(ASFLAGS) $< -o $@
+	
+symbols: $(OBJS)
+	$(READELF) --symbols $<
 
+dump: 
+	$(OBJDUMP) --disassemble-all --target=binary --architecture=m68k \
+		--start-address=0x0000 $(BIN) > $(ROM).dump
 run:
-	$(DEBUGGER) $(BIN) 
+	$(DEBUGGER) -G 640x480 $(BIN)  
 
--- sega.s
+++ sega.s
@@ -2,8 +2,8 @@ rom_start:
 
 	| Sega Genesis ROM header
 	
-	.long	0x00FFE000		| Initial stack pointer value
-	.long	EntryPoint		| Start of our program in ROM
+	.long	0x00FFE000	| Initial stack pointer value
+	.long	EntryPoint	| Start of our program in ROM
 	.long	Interrupt		| Bus error
 	.long	Interrupt		| Address error
 	.long	Interrupt		| Illegal instruction
@@ -70,42 +70,45 @@ rom_start:
 | ******************************************************************************
 | *************************** Sega Genesis ROM Info ****************************
 | ******************************************************************************	
-	.ascii "SEGA MEGA DRIVE "									| Console name
-	.ascii "(C)PASCAL   2000"									| Copyrght
+	.ascii "SEGA MEGA DRIVE "									                | Console name
+	.ascii "(C)PASCAL   2000"									                | Copyrght
 	.ascii "NeoZeddo karāanimēshon                          "	| Domestic name
 	.ascii "NEOZEED COLOR ANIMATION                         "	| International name
-	.ascii "GM XXXXXXXX-XX"										| Version number
-	.word 0x008100B4											| Checksum
-	.ascii "JD              "									| I/O support
-	.long rom_start												| ROM Start Address
-	.long rom_end-1												| ROM End Address
-	.long 0x00FF0000											| RAM Start Address
-	.long 0x00FFFFFF											| RAM End Address
-	.long 0x5241F820											| SRAM enabled
-	.long 0x00200000											| SRAM Start Address
-	.long 0x00200200											| SRAM End Address
-	.ascii "                                        "			| Notes - 52 bytes
-	.ascii "JUE             "									| Region codes (J=Japan, U=USA, E=Europe)
+	.ascii "GM XXXXXXXX-XX"										                | Version number
+	.word 0x008100B4											                    | Checksum
+	.ascii "JD              "									                | I/O support
+	.long rom_start												                    | ROM Start Address
+	.long rom_end-1												                    | ROM End Address
+	.long 0x00FF0000											                    | RAM Start Address
+	.long 0x00FFFFFF											                    | RAM End Address
+	.long 0x5241F820											                    | SRAM enabled
+	.long 0x00200000											                    | SRAM Start Address
+	.long 0x00200200											                    | SRAM End Address
+  .ascii "                                        "			    | Notes - 52 bytes
+	.ascii "JUE             "									                | J=Japan, U=USA, E=Europe
 
-EntryPoint: 						|-- Our code starts here 
+EntryPoint: 						          |-- Our code starts here 
 	tst.l   0x00a10008
 	bne     SkipJoyDetect                               
 	tst.w   0xa1000c
+	
 SkipJoyDetect:
 	bne     SkipSetup
 	lea     Table,a5                       
 	movem.w (a5)+,d5-d7
 	movem.l (a5)+,a0-a4                       
-	move.b  -0x10ff(a1),d0          |Check Version Number                      
+	move.b  -0x10ff(a1),d0               |Check Version Number                      
 	andi.b  #0x000f,d0                             
 	beq     WrongVersion                                   
 	move.l  #0x0053454741,0x002f00(a1)   |Sega Security Code (SEGA)   
+	
 WrongVersion:
 	move.w  (a4),d0
 	moveq   #0x0000,d0                                
 	movea.l d0,a6                                  
 	move    a6,usp                                
 	moveq   #0x0017,d1                | Set VDP registers
+	
 FillLoop:                           
 	move.b  (a5)+,d5
 	move.w  d5,(a4)                              
@@ -114,11 +117,13 @@ FillLoop:
 	move.l  (a5)+,(a4)                            
 	move.w  d0,(a3)                                 
 	move.w  d7,(a1)                                 
-	move.w  d7,(a2)                                 
+	move.w  d7,(a2)        
+	                         
 L0250:
 	btst    d0,(a1)
 	bne     L0250                                   
-	moveq   #0x0025,d2                | Put initial vaules into a00000                
+	moveq   #0x0025,d2                | Put initial vaules into a00000
+	                
 Filla:                                 
 	move.b  (a5)+,(a0)+
 	dbra    d2,Filla
@@ -130,51 +135,54 @@ L0262:
 	dbra    d6,L0262                            
 	move.l  (a5)+,(a4)                              
 	move.l  (a5)+,(a4)                              
-	moveq   #0x001f,d3                | Put initial values into c00000                  
+	moveq   #0x001f,d3                | Put initial values into c00000    
+	              
 Filc0:                             
 	move.l  d0,(a3)
 	dbra    d3,Filc0
 	move.l  (a5)+,(a4)                              
-	moveq   #0x0013,d4                | Put initial values into c00000                 
+	moveq   #0x0013,d4                | Put initial values into c00000    
+	             
 Fillc1:                            
 	move.l  d0,(a3)
 	dbra    d4,Fillc1
-	moveq   #0x0003,d5                | Put initial values into c00011                 
+	moveq   #0x0003,d5                | Put initial values into c00011   
+	              
 Fillc2:                            
 	move.b  (a5)+,0x000011(a3)        
 	dbra    d5,Fillc2                            
 	move.w  d0,(a2)                                 
 	movem.l (a6),d0-d7/a0-a6                    
-	move    #0x002700,sr                           
+	move    #0x002700,sr            
+	               
 SkipSetup:
 	bra     Continue
 Table:
-	dc.w    0x008000, 0x003fff, 0x000100, 0x0000a0, 0x000000, 0x0000a1, 0x001100, 0x0000a1
-	dc.w    0x001200, 0x0000c0, 0x000000, 0x0000c0, 0x000004, 0x000414, 0x00302c, 0x000754
-	dc.w    0x000000, 0x000000, 0x000000, 0x00812b, 0x000001, 0x000100, 0x0000ff, 0x00ff00                                   
-	dc.w    0x000080, 0x004000, 0x000080, 0x00af01, 0x00d91f, 0x001127, 0x000021, 0x002600
-	dc.w    0x00f977, 0x00edb0, 0x00dde1, 0x00fde1, 0x00ed47, 0x00ed4f, 0x00d1e1, 0x00f108                                   
-	dc.w    0x00d9c1, 0x00d1e1, 0x00f1f9, 0x00f3ed, 0x005636, 0x00e9e9, 0x008104, 0x008f01                
-	dc.w    0x00c000, 0x000000, 0x004000, 0x000010, 0x009fbf, 0x00dfff                                
+  .word 0x008000, 0x003fff, 0x000100, 0x0000a0, 0x000000, 0x0000a1, 0x001100, 0x0000a1
+  .word 0x001200, 0x0000c0, 0x000000, 0x0000c0, 0x000004, 0x000414, 0x00302c, 0x000754
+  .word 0x000000, 0x000000, 0x000000, 0x00812b, 0x000001, 0x000100, 0x0000ff, 0x00ff00                                   
+  .word 0x000080, 0x004000, 0x000080, 0x00af01, 0x00d91f, 0x001127, 0x000021, 0x002600
+  .word 0x00f977, 0x00edb0, 0x00dde1, 0x00fde1, 0x00ed47, 0x00ed4f, 0x00d1e1, 0x00f108                                   
+  .word 0x00d9c1, 0x00d1e1, 0x00f1f9, 0x00f3ed, 0x005636, 0x00e9e9, 0x008104, 0x008f01                
+  .word 0x00c000, 0x000000, 0x004000, 0x000010, 0x009fbf, 0x00dfff                                
 
 Continue:
 	tst.w    0x0000C00004
-
-    move.l   #0x000,a7              | set stack pointer
-
+  move.l   #0x000,a7         | set stack pointer
 	move.w  #0x002300,sr       | user mode
-
 	lea     0x00ff0000,a0      | clear Genesis RAM
 	moveq   #0,d0
-clrram: move.w  #0,(a0)+
+	
+clrram: 
+  move.w  #0,(a0)+
 	subq.w  #2,d0
 	bne     clrram
 
-*|----------------------------------------------------------        
-*|
-*|       Load driver into the Z80 memory
-*|
-*|----------------------------------------------------------        
+|----------------------------------------------------------        
+|
+|       Load driver into the Z80 memory
+|
+|----------------------------------------------------------        
 
 	move.w  #0x00100,0x00a11100     | halt the Z80
 	move.w  #0x00100,0x00a11200     | reset it
@@ -191,14 +199,14 @@ Z80loop:
 
 	move.w  #0x000,0x00a11100       | enable the Z80
 
-*|----------------------------------------------------------        
+|----------------------------------------------------------        
 
 	jmp      _main
 
 Interrupt:    
 	rte
 
-*| --- Do nothing for this demo ---
+| --- Do nothing for this demo ---
 HBlank:
 	rte
 
@@ -206,41 +214,42 @@ VBlank:
     addq.l   #1,vtimer
 	rte
 
-*|----------------------------------------------------------        
-*|
-*|       Z80 Sound Driver
-*|
-*|----------------------------------------------------------        
+|----------------------------------------------------------        
+|
+|       Z80 Sound Driver
+|
+|----------------------------------------------------------        
 Z80Driver:
-	  dc.b  0x00c3,0x0046,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
-	  dc.b  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x00f3,0x00ed
-	  dc.b  0x0056,0x0031,0x0000,0x0020,0x003a,0x0039,0x0000,0x00b7
-	  dc.b  0x00ca,0x004c,0x0000,0x0021,0x003a,0x0000,0x0011,0x0040
-	  dc.b  0x0000,0x0001,0x0006,0x0000,0x00ed,0x00b0,0x003e,0x0000
-	  dc.b  0x0032,0x0039,0x0000,0x003e,0x00b4,0x0032,0x0002,0x0040
-	  dc.b  0x003e,0x00c0,0x0032,0x0003,0x0040,0x003e,0x002b,0x0032
-	  dc.b  0x0000,0x0040,0x003e,0x0080,0x0032,0x0001,0x0040,0x003a
-	  dc.b  0x0043,0x0000,0x004f,0x003a,0x0044,0x0000,0x0047,0x003e
-	  dc.b  0x0006,0x003d,0x00c2,0x0081,0x0000,0x0021,0x0000,0x0060
-	  dc.b  0x003a,0x0041,0x0000,0x0007,0x0077,0x003a,0x0042,0x0000
-	  dc.b  0x0077,0x000f,0x0077,0x000f,0x0077,0x000f,0x0077,0x000f
-	  dc.b  0x0077,0x000f,0x0077,0x000f,0x0077,0x000f,0x0077,0x003a
-	  dc.b  0x0040,0x0000,0x006f,0x003a,0x0041,0x0000,0x00f6,0x0080
-	  dc.b  0x0067,0x003e,0x002a,0x0032,0x0000,0x0040,0x007e,0x0032
-	  dc.b  0x0001,0x0040,0x0021,0x0040,0x0000,0x007e,0x00c6,0x0001
-	  dc.b  0x0077,0x0023,0x007e,0x00ce,0x0000,0x0077,0x0023,0x007e
-	  dc.b  0x00ce,0x0000,0x0077,0x003a,0x0039,0x0000,0x00b7,0x00c2
-	  dc.b  0x004c,0x0000,0x000b,0x0078,0x00b1,0x00c2,0x007f,0x0000
-	  dc.b  0x003a,0x0045,0x0000,0x00b7,0x00ca,0x004c,0x0000,0x003d
-	  dc.b  0x003a,0x0045,0x0000,0x0006,0x00ff,0x000e,0x00ff,0x00c3
-	  dc.b  0x007f,0x0000
+  .byte  0x00c3,0x0046,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+  .byte  0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x00f3,0x00ed
+  .byte  0x0056,0x0031,0x0000,0x0020,0x003a,0x0039,0x0000,0x00b7
+  .byte  0x00ca,0x004c,0x0000,0x0021,0x003a,0x0000,0x0011,0x0040
+  .byte  0x0000,0x0001,0x0006,0x0000,0x00ed,0x00b0,0x003e,0x0000
+  .byte  0x0032,0x0039,0x0000,0x003e,0x00b4,0x0032,0x0002,0x0040
+  .byte  0x003e,0x00c0,0x0032,0x0003,0x0040,0x003e,0x002b,0x0032
+  .byte  0x0000,0x0040,0x003e,0x0080,0x0032,0x0001,0x0040,0x003a
+  .byte  0x0043,0x0000,0x004f,0x003a,0x0044,0x0000,0x0047,0x003e
+  .byte  0x0006,0x003d,0x00c2,0x0081,0x0000,0x0021,0x0000,0x0060
+  .byte  0x003a,0x0041,0x0000,0x0007,0x0077,0x003a,0x0042,0x0000
+  .byte  0x0077,0x000f,0x0077,0x000f,0x0077,0x000f,0x0077,0x000f
+  .byte  0x0077,0x000f,0x0077,0x000f,0x0077,0x000f,0x0077,0x003a
+  .byte  0x0040,0x0000,0x006f,0x003a,0x0041,0x0000,0x00f6,0x0080
+  .byte  0x0067,0x003e,0x002a,0x0032,0x0000,0x0040,0x007e,0x0032
+  .byte  0x0001,0x0040,0x0021,0x0040,0x0000,0x007e,0x00c6,0x0001
+  .byte  0x0077,0x0023,0x007e,0x00ce,0x0000,0x0077,0x0023,0x007e
+  .byte  0x00ce,0x0000,0x0077,0x003a,0x0039,0x0000,0x00b7,0x00c2
+  .byte  0x004c,0x0000,0x000b,0x0078,0x00b1,0x00c2,0x007f,0x0000
+  .byte  0x003a,0x0045,0x0000,0x00b7,0x00ca,0x004c,0x0000,0x003d
+  .byte  0x003a,0x0045,0x0000,0x0006,0x00ff,0x000e,0x00ff,0x00c3
+  .byte  0x007f,0x0000
+  
 Z80DriverEnd:
 
 
